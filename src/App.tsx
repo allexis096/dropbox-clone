7c884f13a8b2f5f641c40768e89a3a9f4b4f4b5c
allexis096 <allexissoares@gmail.com>
2020-08-12 20:25:52 -0300

    feat: component Section add to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+
+import Section from './components/Section';
+
 import GlobalStyles from './styles/GlobalStyles';
+import data from './data';
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Section  
+        variants="blue"
+        title={data[0].title}
+        description={data[0].description}
+      />
+      <Section  
+        variants="beige"
+        title={data[1].title}
+        description={data[1].description}
+      />
+      <Section  
+        variants="blue"
+        title={data[2].title}
+        description={data[2].description}
+      />
+      <Section  
+        variants="white"
+        title={data[3].title}
+        description={data[3].description}
+      />
+      <Section  
+        variants="black"
+        title={data[4].title}
+        description={data[4].description}
+      />
       <GlobalStyles />
     </>
   );
